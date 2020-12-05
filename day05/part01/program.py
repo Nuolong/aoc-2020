c5c48118a3b398fa4114cb83be0242c48526f7de
Nuolong <nmcshea@nd.edu>
2020-12-05 01:06:35 -0500

    part01 - ugly code don't judge me :(

---
+++ day05/part01/program.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+
+def findRC(locs):
+    upper = 127
+    lower = 0
+    upperC = 7
+    lowerC = 0
+    row, col = 0, 0
+
+    for letter in locs:
+        if letter == 'F':
+            upper = lower + (upper - lower) // 2
+        elif letter == 'B':
+            lower = math.ceil((upper + lower) / 2)
+        elif letter == 'L':
+            upperC = lowerC + (upperC - lowerC) // 2
+        elif letter == 'R':
+            lowerC = math.ceil((upperC + lowerC) / 2)
+
+    if locs[6] == 'F':
+        row = lower
+    else:
+        row = upper
+
+    if locs[-1] == 'L':
+        col = lowerC
+    else:
+        col = upperC
+
+    return 8 * row + col
+
+def main():
+    locList = [i.strip() for i in sys.stdin]
+
+    maximum = 0
+    for loc in locList:
+        maximum = max(maximum, findRC(loc))
+
+    print(maximum)
+
+if __name__ == '__main__':
+    main()
