68a74d4af1388635c6b3d853c60e108c0aa9ff74
Nuolong <nmcshea@nd.edu>
2020-12-06 00:19:44 -0500

    whoops, only a main

---
+++ day06/part01/program.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+
+def main():
+    group = set()
+    total = 0
+
+    for i in sys.stdin:
+        if i == "\n":
+            total += len(group)
+            print(group)
+            group = set()
+
+        for j in i.split():
+            group = group | {char for char in j}
+
+    # for last input
+    total += len(group)
+    print(total)
+
+
+if __name__ == '__main__':
+    main()
